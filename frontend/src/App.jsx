4efbe25b75bda8f7ee5309d72ee2ad9fe503cb4b
Elodie0530 <uneondine@gmail.com>
2023-07-20 09:33:45 +0200

    create end test navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-
 import "./App.css";
+import Navbar from "./components/Navbar";
+import ChapterList from "./components/ChapterList";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <p>Hello World Reims</p>
+      <ChapterList />
     </div>
   );
 }
