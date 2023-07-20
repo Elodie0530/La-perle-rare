a6cc60423dfc2b70eae261773cdb32b46351dfec
Elodie0530 <uneondine@gmail.com>
2023-07-20 16:23:44 +0200

    add links beetwen chapters

--- backend/database.sql
+++ backend/database.sql
@@ -45,15 +45,13 @@ Etes-vous prêt à vous lancer à sa recherche ? Par où commencer :", "/la_tave
 
 ("Le Village", NULL, NULL, false),
 
-("La Forêt", "La forêt :
-
+("La Forêt", "
 Vous vous enfoncez au coeur de la forêt qui se densifie de plus en plus.
 A un embranchement, vous pouvez aller soit vers :", "/la_foret.jpg", false),
 
 ("Le Château", NULL, NULL, false),
 
-("Le Lac", "Le Lac :
-
+("Le Lac", "
 Après quelques heures de marche, vous arrivez au bord d'un immense lac d'un bleu azur.
 Sur la plage, se trouvent des constructions en bois avec des personnes à proximités.
 Il y a également quelques navires d'amarrer au ponton.
@@ -61,23 +59,21 @@ Où aller vous ?", "/le_lac.jpg", false),
 
 ("La Cabane", NULL, NULL, false),
 
-("Le Navire", "Le Navire :
-
+("Le Navire", "
 Vous vous approchez du ponton, et vous apprêtez à monter à bord du plus gros des navires.
 Lorsque l'on vous interpelle : `Moussaillon, cela peut être très dangereux, d'oser s'aventurer sur ce lac` 
 Que faîte vous ?", "/le_navire.jpg", false),
 
 ("On Descend", NULL, NULL, false),
 
-("On Monte", "Monte :
-
+("On Monte", "
 Une fois monter à bord, le capitaine du navire vous présente une carte du lac. Et il fait des kilomètres et des kilomètres de diamètres.
 Après avoir discuté avec celui-ci, il se trouve que ce lac contient beaucoup d'espèces différentes dont des huîtres géantes.
 Quelle direction prenez-vous ?", "/monte.jpg", false),
 
 ("Les Huîtres Géantes", NULL, NULL, false),
 
-("Autre direction", "Autre direction , Le Kraken :
+("Le Kraken", "
 
 Une fois monter à bord du navire, vous vous dirigez vers le centre du lac, 
 et l'eau commence à s'agiter et change de couleur pour devenir d'un bleu électrique.
@@ -86,7 +82,7 @@ Le navire ralenti, un tourbillon se forme, et voici qu'apparaît des tentacules.
 
 ("Les Naïades", "Après un combat acharné, ou vous sortez vainqueur, apparaît des Naïades, ces déesses aquatiques protectrices de leur milieu 
 `Vous avez fait preuve de beaucoup de courage lors de votre périple Aventurier pour vous récompenser, voici une perle d'une grande rareté, prenez en soin, elle pourrait vous servir.`
-Serait-ce de cette fameuse perle, dont j'avais ouï dire à la taverne, il y a de cela plusieurs lunes ?.", "/la_perle.png", false);
+Serait-ce de cette fameuse perle, dont j'avais ouï dire à la taverne, il y a de cela plusieurs lunes ?", "/la_perle.png", false);
 
 CREATE TABLE lead_to (
   id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -97,5 +93,37 @@ CREATE TABLE lead_to (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO lead_to (name, chapter_start_id, chapter_end_id) VALUES
-("oui", (select id from chapter where title="La Taverne"), (select id from chapter where title="La Forêt")),
-("non", (select id from chapter where title="La Taverne"), (select id from chapter where title="Le Village"));
+("La Forêt", (select id from chapter where title="La Taverne"), (select id from chapter where title="La Forêt")),
+("Le Village", (select id from chapter where title="La Taverne"), (select id from chapter where title="Le Village")),
+
+("Vous êtes sûr ?", (select id from chapter where title="Le Village"), (select id from chapter where title="Perdu")),
+(":(", (select id from chapter where title="Perdu"), (select id from chapter where title="La Taverne")),
+
+("Le Château", (select id from chapter where title="La Forêt"), (select id from chapter where title="Le Château")),
+("Le Lac", (select id from chapter where title="La Forêt"), (select id from chapter where title="Le Lac")),
+
+("Vous êtes sûr ?", (select id from chapter where title="Le Château"), (select id from chapter where title="Perdu")),
+(":(", (select id from chapter where title="Perdu"), (select id from chapter where title="La Taverne")),
+
+("La Cabane", (select id from chapter where title="Le Lac"), (select id from chapter where title="La Cabane")),
+("Le Navire", (select id from chapter where title="Le Lac"), (select id from chapter where title="Le Navire")),
+
+("Vous êtes sûr ?", (select id from chapter where title="La Cabane"), (select id from chapter where title="Perdu")),
+(":(", (select id from chapter where title="Perdu"), (select id from chapter where title="La Taverne")),
+
+("On Descend", (select id from chapter where title="Le Navire"), (select id from chapter where title="On Descend")),
+("On Monte", (select id from chapter where title="Le Navire"), (select id from chapter where title="On Monte")),
+
+("Vous êtes sûr ?", (select id from chapter where title="On Descend"), (select id from chapter where title="Perdu")),
+(":(", (select id from chapter where title="Perdu"), (select id from chapter where title="La Taverne")),
+
+("Autres directions", (select id from chapter where title="On Monte"), (select id from chapter where title="Le Kraken")),
+("Les Huîtres Géantes", (select id from chapter where title="On Monte"), (select id from chapter where title="Les Huîtres Géantes")),
+
+("Vous êtes sûr ?", (select id from chapter where title="Les Huîtres Géantes"), (select id from chapter where title="Perdu")),
+(":(", (select id from chapter where title="Perdu"), (select id from chapter where title="La Taverne")),
+
+("On combat ?", (select id from chapter where title="Le Kraken"), (select id from chapter where title="Les Naïades")),
+("A l'aventure", (select id from chapter where title="Le Kraken"), (select id from chapter where title="Les Naïades")),
+
+("Félicitations", (select id from chapter where title="Les Naïades"), (select id from chapter where title="La Taverne"));
